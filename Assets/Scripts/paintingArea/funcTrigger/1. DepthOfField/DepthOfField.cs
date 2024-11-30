0f34daeed1eb531a8a7be9ef8d39d44f0749c626
minh18ndh <minh.nguyendohoang@gmail.com>
2024-12-01 02:03:10 +0700

    Assign depth of field effect to a button

--- Assets/Scripts/paintingArea/funcTrigger/1. DepthOfField/DepthOfField.cs
+++ Assets/Scripts/paintingArea/funcTrigger/1. DepthOfField/DepthOfField.cs
@@ -1,9 +1,43 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class DepthOfField : MonoBehaviour
 {
+    [SerializeField] private Volume globalVolume;
+    private UnityEngine.Rendering.Universal.DepthOfField depthOfField;
+
+    void Start()
+    {
+        if (globalVolume != null && globalVolume.sharedProfile != null)
+        {
+            // Try to get the DepthOfField component from the profile
+            if (globalVolume.sharedProfile.TryGet(out depthOfField))
+            {
+                Debug.Log("Depth of Field component found in the assigned Global Volume.");
+            }
+            else
+            {
+                Debug.LogError("Depth of Field component not found in the Global Volume profile.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Global Volume is not assigned or has no Volume Profile.");
+        }
+    }
+
     public void ExecuteFunction()
     {
-        Debug.Log("Function DepthOfField executed!");
+        if (depthOfField != null)
+        {
+            depthOfField.focusDistance.Override(10f);
+            depthOfField.focalLength.Override(200f);
+            Debug.Log("Depth of Field effect activated.");
+        }
+        else
+        {
+            Debug.LogError("DepthOfField component is missing.");
+        }
     }
 }
