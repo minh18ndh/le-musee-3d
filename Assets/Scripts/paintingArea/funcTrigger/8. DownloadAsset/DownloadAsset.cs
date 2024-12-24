ef78a84e78eff9fbb654f9636e52ee571df4c388
minh18ndh <minh.nguyendohoang@gmail.com>
2024-12-25 03:54:27 +0700

    Clean up

--- Assets/Scripts/paintingArea/funcTrigger/8. DownloadAsset/DownloadAsset.cs
+++ Assets/Scripts/paintingArea/funcTrigger/8. DownloadAsset/DownloadAsset.cs
@@ -3,68 +3,22 @@ using System.IO;
 
 public class DownloadAsset : MonoBehaviour
 {
-    // File name
-    //public string hostedFileName;
-
     // Hosted file path
     public string hostedFilePath;
-
-    // Local file path
-    //public string localFilePath = @"E:\le-musee-3d\Assets\Models\Paintings\PoolwithTwoFigures.jpg";
-
-    // Local path where the file will be saved (for Unity Editor/Standalone)
-    /*private string savePathforEditororStandaloneBuilds;
-
-    void Start()
-    {
-        // Define the save path for Editor or Standalone builds
-        savePathforEditororStandaloneBuilds = Path.Combine(@"C:\Users\minhn\Downloads", "painting.jpg");
-    }*/
 
     public void ExecuteFunction()
     {
         UIManager.Instance.ShowNotification("Opening asset's link for download...");
         Debug.Log("Download Asset executed.");
-/*#if UNITY_EDITOR || UNITY_STANDALONE
-        // In Editor or Standalone builds, perform the file operations
-        LoadAndSaveLocalFile();*/
-//#elif UNITY_WEBGL
+
         // In WebGL, download using JavaScript
         DownloadFileForWebGL();
-//#endif
     }
 
     public void HaltFunction()
     {
 
     }
-
-/*#if UNITY_EDITOR || UNITY_STANDALONE
-    private void LoadAndSaveLocalFile()
-    {
-        if (File.Exists(localFilePath))
-        {
-            try
-            {
-                // Load the image file as bytes
-                byte[] imageBytes = File.ReadAllBytes(localFilePath);
-
-                // Save the image to the defined path
-                File.WriteAllBytes(savePathforEditororStandaloneBuilds, imageBytes);
-
-                Debug.Log("Image successfully copied to: " + savePathforEditororStandaloneBuilds);
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError("Failed to process image: " + e.Message);
-            }
-        }
-        else
-        {
-            Debug.LogError("Local image file does not exist: " + localFilePath);
-        }
-    }
-#endif*/
 
 //#if UNITY_WEBGL
     private void DownloadFileForWebGL()
